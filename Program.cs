49c6ad6b75f9e51298b032d6e40d043afe48e738
Anton Dmitriev <antondmit@gmail.com>
2022-10-11 01:47:06 +0300

    Задача 6
    Напишите программу, которая на вход принимает число и выдаёт,
    является ли число чётным (делится ли оно на два без остатка).

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// The program calculates parity of a number.
+
+using System;
+class Program
+{
+    // function without any return type declaration  
+    public int GetNumber(string VarName)
+    {
+        int res;
+        string input;
+        do
+        {
+            Console.WriteLine(String.Format("Enter number: {0} = ", VarName));
+            input = Console.ReadLine();
+        } while (int.TryParse(input, out res) == false);
+        return res;
+    }
+
+    public static void Main(string[] args)
+    {
+        Program pr = new Program(); // Creating a class Object  
+        Console.WriteLine("The program calculates parity of a number.");
+
+        string VarName_a = "a";
+        int a = pr.GetNumber(VarName_a);
+        bool is_parity = false;
+        if (a % 2 == 0) {
+            is_parity = true;
+        }
+        
+        Console.WriteLine(String.Format("The value {0} is {1}", a, is_parity));
+    }
+}
